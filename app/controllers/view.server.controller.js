89c9b4ca6c180a99728bb8ab63fd2cd9a337a4fe
Jansen Price <sumpygump@gmail.com>
2015-11-16 17:34:55 -0600

    Update title to match page or buckyt title

--- app/controllers/view.server.controller.js
+++ app/controllers/view.server.controller.js
@@ -3,7 +3,7 @@ var Buckyt = require('mongoose').model('Buckyt');
 exports.render = function(req, res) {
 	Buckyt.findOne({'id' : req.params.buckytId}, function(err, buckyt){
 		res.render('view', {
-        	title: 'View',
+        	title: buckyt.title,
         	activePage: 'View',
         	buckyt: buckyt
     	})
